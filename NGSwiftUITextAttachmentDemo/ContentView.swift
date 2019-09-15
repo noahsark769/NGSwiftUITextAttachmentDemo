7f4cbc60dfa7cb8e4e8c495a1847cbdb3d989524
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-09-15 12:31:56 -0700

    Initial Commit

---
+++ NGSwiftUITextAttachmentDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NGSwiftUITextAttachmentDemo
+//
+//  Created by Noah Gilmore on 9/15/19.
+//  Copyright © 2019 Noah Gilmore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
